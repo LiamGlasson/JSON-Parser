600b7e455b6802810659af137a64e2eed3046a07
Liam Glasson <liam.1000@talktalk.net>
2021-11-25 21:33:38 +0000

    Initial commit

---
+++ src/main/JSONParser.java
@@ -0,0 +1,14 @@
+/*  ╔═══════════════════════════╗
+    ║ @author:     Liam Glasson ║
+    ║ @version:      JDK 17.0.1 ║
+    ╠═══════════════════════════╣
+    ║ @framework:    JSON, Java ║
+    ║ Parser Functionality Tool ║
+    ╚═══════════════════════════╝   */
+package main;
+
+class JSONParser {
+    public static void main(String[] args) {
+        System.exit(0);
+    }
+}
---
+++ src/test/JSONParserTest.java
@@ -0,0 +1,18 @@
+/*  ╔═══════════════════════════╗
+    ║ @author:     Liam Glasson ║
+    ║ @version:      JDK 17.0.1 ║
+    ╠═══════════════════════════╣
+    ║ @framework:    JSON, Java ║
+    ║ Parser Functionality Test ║
+    ╚═══════════════════════════╝   */
+package test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class JSONParserTest {
+    @Test
+    public void initialise() {
+        System.out.println("Initial Test");
+    }
+}
